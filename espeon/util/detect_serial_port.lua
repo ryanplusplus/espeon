d5d91f8c285616404835dae5245f58d724e6d304
Ryan Hartlage <ryanplusplus@gmail.com>
2018-04-28 15:44:49 -0400

    v0.5-0

--- espeon/util/detect_serial_port.lua
+++ espeon/util/detect_serial_port.lua
@@ -4,7 +4,11 @@ return function()
   if os.getenv('SERIAL_PORT') then
     return os.getenv('SERIAL_PORT')
   else
-    local device_name = shell('ls /dev/ | grep -i tty | grep -m1 -i usb')
+    local device_name = shell('ls /dev/ | grep -i tty | grep -m1 -i usb'):gsub('\n', '')
+    if device_name == '' then
+      print('Unable to detect serial port. Is your device connected?\n')
+      os.exit(1)
+    end
     return '/dev/' .. device_name:gsub('\n', '')
   end
 end

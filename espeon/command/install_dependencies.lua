38fba3df2f9580eb0266d0efe35b886eb37ec54a
Ryan Hartlage <ryanplusplus@gmail.com>
2020-06-13 10:09:26 -0400

    Improved install_dependencies

--- espeon/command/install_dependencies.lua
+++ espeon/command/install_dependencies.lua
@@ -1,6 +1,8 @@
 local exec = require 'espeon.util.exec'
 local detect_platform = require 'espeon.util.detect_platform'
 
+local nodemcu_tool = 'nodemcu-tool@3.2.1'
+
 return {
   description = 'Install all external dependencies',
 
@@ -10,10 +12,10 @@ return {
     if platform == 'linux' then
       exec({
         'sudo apt-get install screen',
-        'sudo apt-get install python-pip',
-        'sudo -H pip install esptool --upgrade',
+        'sudo apt-get install python-pip || sudo apt-get install python3-pip',
+        'sudo -H pip install esptool --upgrade || sudo -H pip3 install esptool --upgrade',
         'which npm > /dev/null || sudo apt install nodejs',
-        'npm install -g nodemcu-tool || sudo npm install -g nodemcu-tool'
+        'npm install -g ' .. nodemcu_tool .. ' || sudo npm install -g ' .. nodemcu_tool
       })
     elseif platform == 'mac' then
       exec({
@@ -21,7 +23,7 @@ return {
         'sudo pip install esptool --upgrade',
         'which brew > /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"',
         'which npm > /dev/null || brew install node',
-        'npm install -g nodemcu-tool || sudo npm install -g nodemcu-tool'
+        'npm install -g ' .. nodemcu_tool .. ' || sudo npm install -g ' .. nodemcu_tool
       })
     end
   end
